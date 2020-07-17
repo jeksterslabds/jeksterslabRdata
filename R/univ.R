7828d0cb062f984c7ed93087387eb9d06ad80d9d
Ivan Jacob Agaloos Pesigan <learn.jeksterslab@gmail.com>
2020-07-17 14:00:57 +0800

    Edits to documentation.

--- R/univ.R
+++ R/univ.R
@@ -1,108 +1,49 @@
-#' Generate Univariate Data
+#' @author Ivan Jacob Agaloos Pesigan
+#'
+#' @title Generate Univariate Data
 #'
 #' @description Generates an \eqn{n \times 1} univariate data vector
-#' or a list of \eqn{n \times 1} univariate data vectors of length `R`.
-#' The default data generating function
-#' is the normal distribution
-#' \deqn{
-#'   X
-#'   \sim
-#'   \mathcal{N}
-#'   \left(
-#'     \mu,
-#'     \sigma^2
-#'   \right) .
-#'   %(\#eq:dist-X-norm)
-#' }
+#'   or a list of \eqn{n \times 1} univariate data vectors of length `R`.
+#'   The default data generating function
+#'   is the normal distribution
+#'   \deqn{
+#'     X \sim \mathcal{N} \left( \mu, \sigma^2 \right) .
+#'     %(\#eq:dist-X-norm)
+#'   }
 #'
 #' @details The univariate distribution and parameters used
-#' in the data generating process
-#' can be specified using `rFUN` and `...`.
+#'   in the data generating process can be specified using `rFUN` and `...`.
 #'
-#' Options for explicit parallelism are provided
-#' when `R > 1` especially when `R` is large.
-#' See `par` and suceeding arguments.
+#'   Options for explicit parallelism are provided when `R > 1`
+#'   especially when `R` is large. See `par` and suceeding arguments.
 #'
-#' @author Ivan Jacob Agaloos Pesigan
+#' @return If `R = NULL` or `R = 1`,
+#'   returns an \eqn{ n \times 1 } univariate data vector generated
+#'   using `rFUN` and parameters passed to `...`.
+#'   If `R` is an integer greater than 1, (e.g., `R = 10`)
+#'   returns a list of length `R` of \eqn{ n \times 1 } univariate data vector
+#'   generated using `rFUN` and parameters passed to `...`.
+#'
 #' @family univariate data functions
 #' @keywords univariate
 #' @inheritParams jeksterslabRpar::par_lapply
-#' @param n Integer.
-#' Sample size.
-#' @param rFUN Function.
-#' Data generating function to generate univariate data.
-#' @param ... Arguments to pass to `rFUN`.
-#' @param R Integer.
-#' Number of Monte Carlo replications.
-#' If `R` is not provided,
-#' the function produces a single random data set.
-#' If `R` is an integer greater than 1,
-#' (e.g., `R = 10`),
-#' the function produces
-#' multiple random data sets
-#' stored in each element of a list of length `R`.
-#' `par` and all succeeding arguments are only relevant
-#' when `R > 1`.
-#' @return
-#' If `R = NULL` or `R = 1`,
-#' returns an
-#' \eqn{
-#'   n \times 1
-#' }
-#' univariate data vector
-#' generated using `rFUN` and parameters passed to `...`.
-#' If `R` is an integer greater than 1,
-#' (e.g., `R = 10`)
-#' returns a list of length `R` of
-#' \eqn{
-#'   n
-#'   \times 1
-#' }
-#' univariate data vector
-#' generated using `rFUN` and parameters passed to `...`.
-#' @examples
-#' n <- 1000
-#' R <- 5
-#' # normal distribution---------------------------------------------------------
-#' mu <- 0
-#' sigma2 <- 1
-#' sigma <- sqrt(sigma2)
-#' x <- univ(n = n, rFUN = rnorm, mean = mu, sd = sigma)
-#' str(x)
-#' hist(x, main = "Normal Distribution")
-#' x <- univ(n = n, rFUN = rnorm, R = R, mean = mu, sd = sigma)
-#' str(x)
-#' # bernoulli distribution------------------------------------------------------
-#' n_trials <- 1
-#' p <- 0.50
-#' x <- univ(n = n, rFUN = rbinom, size = n_trials, prob = p)
-#' str(x)
-#' barplot(table(x), main = "Bernoulli Distribution")
-#' x <- univ(n = n, rFUN = rbinom, R = R, size = n_trials, prob = p)
-#' str(x)
-#' # binomial distribution-------------------------------------------------------
-#' n_trials <- 20
-#' p <- 0.50
-#' x <- univ(n = n, rFUN = rbinom, size = n_trials, prob = p)
-#' barplot(table(x), main = "Binomial Distribution")
-#' str(x)
-#' x <- univ(n = n, rFUN = rbinom, R = R, size = n_trials, prob = p)
-#' str(x)
-#' # exponential distribution----------------------------------------------------
-#' rate <- 1
-#' x <- univ(n = n, rFUN = rexp, rate = rate)
-#' hist(x, main = "Exponential Distribution")
-#' str(x)
-#' x <- univ(n = n, rFUN = rexp, R = R, rate = rate)
-#' str(x)
-#' # rbind = TRUE----------------------------------------------------------------
-#' set.seed(42)
-#' univ(n = 5, rFUN = rnorm, R = 5, rbind = TRUE, mean = mu, sd = sigma)
-#' # rbind = FALSE---------------------------------------------------------------
-#' set.seed(42)
-#' univ(n = 5, rFUN = rnorm, R = 5, rbind = FALSE, mean = mu, sd = sigma)
 #' @importFrom stats rnorm
 #' @importFrom jeksterslabRpar par_lapply
+#' @param n Integer.
+#'   Sample size.
+#' @param rFUN Function.
+#'   Data generating function to generate univariate data.
+#' @param ... Arguments to pass to `rFUN`.
+#' @param R Integer.
+#'   Number of Monte Carlo replications. If `R` is not provided,
+#'   the function produces a single random data set.
+#'   If `R` is an integer greater than 1, (e.g., `R = 10`),
+#'   the function produces multiple random data sets
+#'   stored in each element of a list of length `R`.
+#'   `par` and all succeeding arguments are only relevant when `R > 1`.
+#' @examples
+#' x <- univ(n = 1000, rFUN = rnorm, mean = 100, sd = sqrt(225))
+#' xstar <- univ(n = 1000, rFUN = rnorm, mean = 100, sd = sqrt(225), R = 1000)
 #' @export
 univ <- function(n, # rFUN
                  rFUN = rnorm,
